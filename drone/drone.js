60fdca62ca2164c5da68e4af124c34cd986daf97
Malik Rahey <mjrahey@mun.ca>
2023-02-02 20:30:30 -0330

    added untested code for camera output

--- drone/drone.js
+++ drone/drone.js
@@ -3,7 +3,17 @@ const url = require('url')
 const arDone = require('ar-drone')
 
 const client = arDone.createClient();
-console.log(client);
+const pngStream = client.getPngStream();
+let lastPng;
+pngStream.on('data', buffer => {
+	lastPng = buffer;
+})
+.on('error', err => {
+	console.log('png stream error');
+	console.log(err);
+})
+
+
 const drone = {
   client,
   server: () => {
@@ -63,7 +73,15 @@ const drone = {
 
 const server = http.createServer((req, res) => {
 	
-	const query = url.parse(req.url).query;
+	if(req.url === "/camera") {
+		if (lastPng) {
+			res.writeHead('200', {'Content-Type': 'image/png'});
+			res.end(lastPng);
+		} else {
+			res.writeHead(503);
+			res.end('No stream data recieved.');
+		}
+	}
 
 	console.log(req.url);
 
