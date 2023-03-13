13eed3b828051121443cc1d7c55879ed7fd93c94
Jarrod Rumsey <jrumsey@mun.ca>
2023-03-13 02:41:59 -0230

    Fixed keyboard bug

--- src/keyboard.js
+++ src/keyboard.js
@@ -21,22 +21,28 @@ function KeyEnd() {
 
 //TODO: Update to use ctrl+{key} inputs only.
 
+let key_active = false; //Stops button presses from "repeating" while being held down.
+
 function KeyDown(event) {
     //document.getElementById(`last-keypress`).innerText = event.key + " Pressed";
 
     let button = document.getElementById(`kb-${event.key}`);
     button.style = "background-color: var(--btn-kb-active);";
     //TODO: Start button held action. Will need something like KeyStart above.
-    button.click();
+    if(!key_active)
+    {
+        key_active = true;
+        button.click();
+    }
 }
 
 function KeyUp(event) {
     //document.getElementById(`last-keypress`).innerText = event.key + " Released";
+    key_active = false;
 
     let button = document.getElementById(`kb-${event.key}`);
     button.style = 'none'
     //TODO: Stop button held action. Will need something like KeyStop above.
-    button.click();
 }
 
 window.addEventListener('keydown', KeyDown, true);
