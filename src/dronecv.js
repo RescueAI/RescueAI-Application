19408cc8d72d45680b359603f1276be6e782c395
Malik Rahey <mjrahey@mun.ca>
2023-03-13 08:49:27 -0230

    added code for frontend based predictions

--- src/dronecv.js
+++ src/dronecv.js
@@ -1,9 +1,10 @@
-
+//import * as tf from "@tensorflow/tfjs";
 // const cocoSsd = window['coco-ssd'];
 // console.log(cocoSsd);
 let cvReady = window.cvReady;
 const canvas = document.getElementById("image-output");
-
+let camera;
+const tf = window.tfjs;
 const FPS = 30;
 const height = 320;
 const width = 480;
@@ -18,24 +19,47 @@ document.getElementById("camera").onclick = () => {
   processVideo();
 };
 
-function processVideo() {
+async function initTf() {
+  camera = await tf.data.webcam(document.createElement('video')) 
+  let image = await camera.capture();
+  image.print();
+  camera.stop();
+}
+
+async function processVideo() {
   console.log("processing video");
   lastImg = undefined;
-  fetch("http://localhost:6969/camera")
-    .then((data) => {
-      console.log(data)
-      return data.blob();
-    })
-    .then((blob) => {
-      console.log(blob);
-      img = URL.createObjectURL(blob);
-      console.log(img)
-      lastImg = img;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  document.getElementById("image-output").setAttribute("src", img);;
+  camera = await tf.data.webcam(document.createElement('video')) 
+  let image = await camera.capture().cast('int32').expandDims();
+  //image = image.reshape([1,360, 640, 3])
+  const model = await tf.loadGraphModel('./models/model.json');
+
+  const predictions = await model.executeAsync(image);
+  
+  console.log(predictions);
+  const classes = predictions[0].shape[2];
+  console.log(classes);
+
+
+
+  // fetch("http://localhost:6969/camera")
+  //   .then((data) => {
+  //     console.log(data)
+  //     return data.blob();
+  //   })
+  //   .then((blob) => {
+  //     console.log(blob);
+  //     img = URL.createObjectURL(blob);
+  //     console.log(img)
+  //     lastImg = img;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // document.getElementById("image-output").setAttribute("src", img);;
+
+
+
   // const context = canvas.getContext("2d");
   // let outImg = new Image();
   // outImg.src = img;
@@ -61,7 +85,7 @@ function processVideo() {
 
   mat.delete();
 }
-
+initTf();
 //setInterval(processVideo, 50);
 
 //openCvReady();
