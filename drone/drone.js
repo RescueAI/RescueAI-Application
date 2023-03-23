34e97af5b72078f91dfae708dbddaf8984ddbac8
rjcarolan <54218447+rjcarolan@users.noreply.github.com>
2023-03-23 15:30:38 -0230

    fetch

--- drone/drone.js
+++ drone/drone.js
@@ -1,6 +1,8 @@
 
 const http = require('http')
 const url = require('url')
+var autonomy = require('ardrone-autonomy');
+var mission = autonomy.createMission();
 const arDone = require('ar-drone')
 const cv = require('../opencv');
 const fs = require('fs');
@@ -10,6 +12,7 @@ const client = arDone.createClient();
 const axios = require('axios')
 const FormData = require("form-data")
 const moment = require("moment");
+
 
 const mlreqOptions = {
 	hostname: "127.0.0.1",
@@ -35,7 +38,7 @@ let MISSION_START_TIME;
 
 
 let recentEvent = {
-	message:"",
+	message: "",
 	image: undefined,
 	detections: 0,
 	timestamp: moment(),
@@ -56,23 +59,23 @@ let model = undefined;
 //initializeTf();
 
 const server = http.createServer((req, res) => {
-	if(req.url === "/camera") {
-		
+	if (req.url === "/camera") {
+
 		webCamCapture();
 		if (lastPng) {
 			try {
 				//res.writeHead('200', {'Content-Type': 'image/png'});
-				
-				
+
+
 				detect(lastPng);
 				lastPng = fs.readFileSync('./src/known.jpg');
-				const data = {image: lastPng, boxes: boxes};
+				const data = { image: lastPng, boxes: boxes };
 				const dataBuffer = Buffer.from(JSON.stringify(data));
 				res.end(lastPng);
 			} catch (e) {
 				console.log(e);
 			}
-			
+
 		} else {
 			res.writeHead(503);
 			res.end('No stream data recieved.');
@@ -83,7 +86,7 @@ const server = http.createServer((req, res) => {
 		lastPng = fs.readFileSync('./src/known.jpg');
 		getPreBoxImage(lastPng);
 		if (res)
-		res.end(preBoxedImg);
+			res.end(preBoxedImg);
 	}
 
 	if (req.url === "/get_boxes") {
@@ -111,8 +114,7 @@ const server = http.createServer((req, res) => {
 
 	//console.log(req.url);
 	//Update missions with new mission set.
-	if(req.url === "/post_missions" && req.method === 'POST') 
-	{
+	if (req.url === "/post_missions" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
 			body += chunk.toString();
@@ -121,14 +123,12 @@ const server = http.createServer((req, res) => {
 		req.on('end', () => {
 			fs.writeFile('./data/missions.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not save mission data");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Mission data saved successfully");
 				}
@@ -137,16 +137,14 @@ const server = http.createServer((req, res) => {
 	}
 
 	//Read current missions
-	if(req.url === "/get_missions" && req.method === 'GET') {
+	if (req.url === "/get_missions" && req.method === 'GET') {
 		fs.readFile('./data/missions.json', (err, data) => {
-			if(err) 
-			{
+			if (err) {
 				console.error(err);
 				res.statusCode = 500;
 				res.end("Error: Could not read mission data");
 			}
-			else
-			{
+			else {
 				res.statusCode = 200;
 				res.setHeader("Content-Type", "application/json");
 				res.end(data);
@@ -154,11 +152,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/forward" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/forward" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -166,14 +163,12 @@ const server = http.createServer((req, res) => {
 			console.log("forward");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone forward");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved forward");
 				}
@@ -182,11 +177,10 @@ const server = http.createServer((req, res) => {
 
 	}
 
-	if(req.url === "/api/drone/move/backward" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/backward" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -194,14 +188,12 @@ const server = http.createServer((req, res) => {
 			console.log("backward");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone backward");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved backward");
 				}
@@ -209,11 +201,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/left" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/left" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -221,14 +212,12 @@ const server = http.createServer((req, res) => {
 			console.log("left");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone left");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved left");
 				}
@@ -236,11 +225,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/right" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/right" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -249,14 +237,12 @@ const server = http.createServer((req, res) => {
 			console.log("right");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone right");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved right");
 				}
@@ -264,11 +250,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/up" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/up" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -276,14 +261,12 @@ const server = http.createServer((req, res) => {
 			console.log("up");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone up");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved up");
 				}
@@ -291,11 +274,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/down" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/down" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -303,14 +285,12 @@ const server = http.createServer((req, res) => {
 			console.log("down");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not move drone down");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully moved down");
 				}
@@ -318,11 +298,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/rotate-left" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/rotate-left" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -330,14 +309,12 @@ const server = http.createServer((req, res) => {
 			console.log("rotate-left");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not rotate drone left");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully rotated left");
 				}
@@ -345,11 +322,10 @@ const server = http.createServer((req, res) => {
 		});
 	}
 
-	if(req.url === "/api/drone/move/rotate-right" && req.method === 'POST') 
-	{
+	if (req.url === "/api/drone/move/rotate-right" && req.method === 'POST') {
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		//TODO: Forward move command here
@@ -357,14 +333,12 @@ const server = http.createServer((req, res) => {
 			console.log("rotate-right");
 			fs.writeFile('./data/commands.json', body, err => {
 
-				if(err) 
-				{
+				if (err) {
 					console.error(err);
 					res.statusCode = 500;
 					res.end("Error: Could not rotate drone right");
 				}
-				else
-				{
+				else {
 					res.statusCode = 200;
 					res.end("Drone successfully rotated right");
 				}
@@ -375,6 +349,33 @@ const server = http.createServer((req, res) => {
 	if (req.url.startsWith('/api/drone/start/') && req.method === 'POST') {
 
 		const missionId = parseInt(req.url.slice('/api/drone/start/'.length));
+		var mission = autonomy.createMission(client);
+		switch (missionId) {
+			case 1:
+				sweep_mission(mission);
+				break;
+			case 2:
+				squareSpiral(mission);
+				break;
+			case 3:
+				square_path(mission);
+				break;
+
+			default:
+				break;
+		};
+		 mission.run(function (err, result) {
+			if (err) {
+				console.trace("Oops, something bad happened: %s", err.message);
+				mission.client().stop();
+				mission.client().land();
+			} else {
+				console.log("Mission success!");
+				process.exit(0);
+			}});
+
+		
+
 		// TODO: Implement logic for starting drone mission with the provided mission ID
 		// Example:
 		//stopCurrentMission()
@@ -383,24 +384,22 @@ const server = http.createServer((req, res) => {
 
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
 
 		req.on('end', () => {
 
-		  if(status == 200)
-		  {
-			MISSION_START_TIME = moment();
-			res.statusCode = 200;
-			res.end('Drone mission started');
-			console.log("Drone mission with ID:"+missionID+" started");
-		  }
-		  else
-		  {
-			res.statusCode = 500;
-			res.end('Drone mission failed to start');
-			console.error("Drone mission failed to start");
-		  }
+			if (status == 200) {
+				MISSION_START_TIME = moment();
+				res.statusCode = 200;
+				res.end('Drone mission started');
+				console.log("Drone mission with ID:" + missionID + " started");
+			}
+			else {
+				res.statusCode = 500;
+				res.end('Drone mission failed to start');
+				console.error("Drone mission failed to start");
+			}
 
 		});
 	}
@@ -409,24 +408,25 @@ const server = http.createServer((req, res) => {
 		// TODO: Implement logic for stopping drone mission
 		// Example: stopDroneMission();
 		let status; //=stopDroneMission(); ///pass by reference a status if possible
+		mission.control().disable();
+        mission.client().land(function() {
+            process.exit(0)});
 
 		let body = '';
 		req.on('data', chunk => {
-			
+
 		})
-		
+
 		console.log("Stopping mission");
 
-		if(status == 200)
-		{
-		  res.statusCode = 200;
-		  res.end('Drone mission stopped');
-		  console.log("Drone mission was stopped");
+		if (status == 200) {
+			res.statusCode = 200;
+			res.end('Drone mission stopped');
+			console.log("Drone mission was stopped");
 		}
-		else
-		{
-		  res.statusCode = 500;
-		  res.end('Drone mission could not be stopped');
+		else {
+			res.statusCode = 500;
+			res.end('Drone mission could not be stopped');
 		}
 	}
 });
@@ -438,9 +438,9 @@ server.listen(6969, () => {
 async function detect(image) {
 	const buf = fs.readFileSync('./src/known.jpg')
 	//fs.writeFileSync('./ml-server/photoDir/photo.jpg', buf)
-	if(image === undefined) return;
+	if (image === undefined) return;
 	try {
-		
+
 		const formData = new FormData();
 		const headers = {
 			...formData.getHeaders()
@@ -451,7 +451,7 @@ async function detect(image) {
 		if (boxes?.boxes) {
 			const currentTime = moment();
 			const difference = currentTime.diff(recentEvent.localtime);
-			
+
 			if (Number(difference.seconds()) > 20) {
 				recentEvent = {
 					image: preBoxedImg,
@@ -465,20 +465,20 @@ async function detect(image) {
 		// const req = http.request(mlreqOptions, res => {
 		// 	res.on('data', data => {
 		// 		boxes = data;
-				
+
 		// 		console.log("data get");
 		// 	})
 		// 	res.on('error', err => {
 		// 		console.log("Error in req to python/ML")
 		// 	})
 		// });
-			
+
 
 
 		// let tfimg = tf.node.decodeJpeg(buf);
 		// tfimg = tfimg.reshape([1,640,360,3]);
 		// console.log(tfimg.shape)
-		
+
 		// const predictions = await model.predict(tfimg);
 		// console.log(`predictions length = ${predictions.length}`);
 		// console.log(predictions);
@@ -489,7 +489,7 @@ async function detect(image) {
 	} catch (e) {
 		console.log(e)
 	}
-	
+
 }
 
 const getPreBoxImage = async (image) => {
@@ -499,7 +499,7 @@ const getPreBoxImage = async (image) => {
 			preBoxedImg = data;
 		})
 	});
-	const data = {image: buf};
+	const data = { image: buf };
 	const dataBuffer = Buffer.from(JSON.stringify(data))
 	req.write(dataBuffer);
 	req.end();
@@ -507,9 +507,9 @@ const getPreBoxImage = async (image) => {
 
 const opts = {
 	width: 1280,
-  height: 720,
-  quality: 100,
-  output: "png",
+	height: 720,
+	quality: 100,
+	output: "png",
 	//callbackReturn: "buffer"
 }
 const webcam = NodeWebcam.create(opts);
@@ -517,9 +517,91 @@ const webcam = NodeWebcam.create(opts);
 async function webCamCapture() {
 	webcam.clear();
 	await webcam.capture("test", (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		lastPng = data;
 	});
 }
 
 //export default drone;
+
+function squareSpiral(mission){
+    mission.takeoff()
+    .zero()
+    .altitude(1)
+    .hover(1000)
+    .forward(1)
+    .cw(90)
+    .forward(1)
+    .cw(90)
+    .forward(2)
+    .cw(90)
+    .forward(2)
+    .cw(90)
+    .forward(3)
+    .cw(90)
+    .forward(3)
+    .cw(90)
+    .forward(4)
+    .cw(90)
+    .forward(4)
+    .cw(90)
+    .forward(4)
+    .cw(90)
+    .forward(4)
+    .go({x:0, y:0})
+    .hover(1000)
+    .land();
+}
+
+function sweep_mission(mission) {
+	mission.takeoff()
+		.zero()
+		.altitude(1)
+		.hover(500)
+		.forward(1)
+		.hover(500)
+		.cw(180)
+		.hover(500)
+		.forward(0.5)
+		.hover(500)
+		.cw(90)
+		.hover(500)
+		.forward(1)
+		.hover(500)
+		.ccw(90)
+		.hover(500)
+		.forward(0.5)
+		.hover(500)
+		.ccw(90)
+		.hover(500)
+		.forward(1)
+		.hover(500)
+		.cw(90)
+		.hover(500)
+		.forward(0.5)
+		.hover(500)
+		.cw(90)
+		.hover(500)
+		.forward(1)
+		.hover(500)
+		.go({ x: 0, y: 0 })
+		.hover(1000)
+		.land();
+}
+
+function square_path(mission){
+	mission.takeoff()
+       .zero()
+       .hover(500)
+       .altitude(2)
+       .forward(2)
+       .cw(90)
+       .forward(2)
+       .cw(90)
+       .forward(2)
+       .cw(90)
+       .forward(2)
+       .go({x:0, y:0})
+       .hover(500)
+       .land();
+}
